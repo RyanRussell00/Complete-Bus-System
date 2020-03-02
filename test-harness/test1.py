e1eeb415cd2b9df7da3eb6baecf719ee5f04cf94
Ryan Russell <Ryan.Russell2000@gmail.com>
2020-03-01 23:06:40 -0800

    Removed some files

--- test-harness/test1.py
+++ test-harness/test1.py
@@ -4,10 +4,36 @@ import re;
 print("mysql-connector Found Successfuly");
 
 
-#Regex to get only the string with dashes, periods, and underscores
+# class Employee(object):
+#     ssn = "";
+#     Fname = "";
+#     Minit = "";
+#     Lname = "";
+#     street = "";
+#     city = "";
+#     state = "";
+#     zip = "";
+#     start_date = "";
+#     supervisor = "";
+#
+#     def __init__(self, ssn, Fname, Minit, Lname, street, city, state, zip, start_date, supervisor):
+#         self.ssn = ssn;
+#         self.Fname = Fname;
+#         self.Minit = Minit;
+#         self.Lname = Lname;
+#         self.street = street;
+#         self.city = city;
+#         self.state = state;
+#         self.zip = zip;
+#         self.start_date = start_date;
+#         self.supervisor = supervisor;
+
+
+# Regex to get only the string with dashes, periods, and underscores
 def formatString(strIn):
-  result = re.sub("[^a-zA-Z0-9_.-]*", "", str(strIn));
-  return result;
+    result = re.sub("[^a-zA-Z0-9_.-]*", "", str(strIn));
+    return result;
+
 
 def testGetTables():
     dbCursor.execute("SHOW TABLES");
@@ -29,8 +55,13 @@ def testGetTables():
 
     return True;
 
-def testInsert():
-  dbCursor.execute()
+#
+# def testInsertEmployees():
+#     Emp = Employee("123456789", "Alex", "A", "Walt", "3234 Blade Road",
+#                   "Seattle", "WA", "98036", "05-22-2001");
+#
+#     dbCursor.execute("INSERT INTO TABLE EMPLOYEE VALUES (" + Emp.ssn + Emp.Fname + Emp.Minit + Emp.Lname +
+#                      Emp.street + Emp.city + Emp.state + Emp.zip + Emp.start_date + Emp.supervisor);
 
 
 # Main
@@ -45,5 +76,7 @@ dbCursor = mydb.cursor();
 # Regex to ignore all non-alphabet characters
 
 print("Tables Exist: " + str(testGetTables()));
+
+# testInsertEmployees();
 
 mydb.close();
