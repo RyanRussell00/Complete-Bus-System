90b1b4ce9e0537160a48a244014de88408267fa5
Ryan Russell <Ryan.Russell2000@gmail.com>
2020-03-01 20:04:21 -0800

    Fixed up the creates

--- Create-Database-V1.sql
+++ Create-Database-V1.sql
@@ -1,99 +1,98 @@
-drop database sys;
-create database sys;
-use sys;
+drop database CBS;
+create database CBS;
+use CBS;
+
+DROP TABLE EMPLOYEE;
+DROP TABLE BUS;
+DROP TABLE CARD;
+DROP TABLE TAPS;
+DROP TABLE FARE_TIER;
+DROP TABLE BUS_STOP;
+DROP TABLE ROUTE;
+DROP TABLE VISIT;
+DROP TABLE SCHEDULED;
+
 CREATE TABLE EMPLOYEE (
-    ID INT(7) NOT NULL UNIQUE AUTO_INCREMENT,
-    FIRST_NAME VARCHAR(15) NOT NULL,
-    MID_INIT VARCHAR(2),
-    LAST_NAME VARCHAR(15) NOT NULL,
-    STREET VARCHAR(20) NOT NULL,
-    CITY VARCHAR(12) NOT NULL,
-    STATE CHAR(2) NOT NULL,
-    ZIP CHAR(5) NOT NULL,
-    SSN INT(9) NOT NULL UNIQUE,
-    START_DATE DATE,
-    SUPERVISOR INT(7),
-    PRIMARY KEY (ID),
-    KEY (SSN)
+	ssn int(9) NOT NULL UNIQUE,
+    Fname varchar(15) NOT NULL,
+    Minit varchar(2),
+    Lname varchar(15) NOT NULL,
+    street varcharR(20) NOT NULL,
+    city varchar(12) NOT NULL,
+    state char(2) NOT NULL,
+    zip char(5) NOT NULL,
+    start_date date,
+    supervisor int(7),
+    PRIMARY KEY (id),
+    KEY (ssn)
 );
 
 CREATE TABLE BUS (
-    ID INT(5) NOT NULL,
-    CAPACITY INT(3) DEFAULT 1,
-    START_DATE DATE,
-    DRIVER INT(7) UNIQUE DEFAULT NULL,
-    PRIMARY KEY (ID),
-    FOREIGN KEY (DRIVER)
-        REFERENCES EMPLOYEE (ID)
-);
-CREATE TABLE CARD (
-    ID INT(10) UNIQUE NOT NULL,
-    BALANCE DECIMAL(5 , 2 ) NOT NULL DEFAULT 0,
-    Expr_Date DATE NOT NULL,
-    PRIMARY KEY (ID)
-);
-CREATE TABLE TAPS (
-    B_ID INT(5) NOT NULL,
-    C_ID INT(10) NOT NULL,
-    Time_Stamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (B_ID , C_ID),
-    FOREIGN KEY (B_ID)
-        REFERENCES BUS (ID),
-    FOREIGN KEY (C_ID)
-        REFERENCES CARD (ID)
+	busId int(5) not null,
+    capacity int(3) default 1,
+    start_date date,
+    E_driver int(7) unique default null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (driver) REFERENCES EMPLOYEE(id)
 );
 CREATE TABLE FARE_TIER (
-    FARE_LEVEL VARCHAR(50) NOT NULL DEFAULT 'Adult fares (age 19-64)',
-    FARE_COST DECIMAL(3 , 2 ) NOT NULL,
-    CARD_ID INT(10) UNIQUE NOT NULL,
-    PRIMARY KEY (FARE_LEVEL , FARE_COST),
-    FOREIGN KEY (CARD_ID)
-        REFERENCES CARD (ID),
-    CONSTRAINT FARE_COST_TYPE CHECK (FARE_COST = (CASE
-        WHEN
-            FARE_LEVEL = 'Children age(0-5)'
-                OR FARE_LEVEL = 'Senior/disabled fares (age 65+ or qualifying disability)'
-        THEN
-            0.00
-        WHEN FARE_LEVEL = 'Youth fares (age 6-18)' THEN 2.75
-        WHEN FARE_LEVEL = 'Adult fares (age 19-64)' THEN 3.25
-    END)),
-    CONSTRAINT FARE_LEVEL CHECK (FARE_LEVEL IN ('Children age(0-5)' , 'Youth fares (age 6-18)',
-        'Senior/disabled fares (age 65+ or qualifying disability)',
-        'Adult fares (age 19-64)'))
+	tier int(2) not null,
+	cost float(4, 2) not null,
+	fare_name varchar(9) default NULL,
+	PRIMARY KEY (tier)
+);
+CREATE TABLE CARD (
+	card_number int(9) not null,
+	balance float(4, 2) default 0,
+	expiry_date DATE,
+	F_fare int(2) not null, 
+	PRIMARY KEY (card_number),
+	CONSTRAINT card_fare FOREIGN KEY (F_fare) REFERENCES FARE_TIER(tier)
+);
+CREATE TABLE TAPS (
+	B_busId int(5) not null,
+	C_card_num int(9) not null,
+	time_stamp DATETIME not null,
+	PRIMARY KEY (bus_id, card_num, time_stamp)
+	CONSTRAINT tap_bus FOREIGN KEY (B_busId) REFERENCES BUS(id);
+	CONSTRAINT tap_card FOREIGN KEY (C_card_num) REFERENCES CARD(card_number);
 );
 CREATE TABLE BUS_STOP(
-	ID INT(4) UNIQUE NOT NULL,
-    NAME VARCHAR(50) NOT NULL,
-    STREET1 VARCHAR(50) NOT NULL,
-    STREET2 VARCHAR(50) NOT NULL,
-    PRIMARY KEY (ID)
+	stopId int(6) not null,
+	stop_name varchar(20) default NULL,
+	street1 varchar(20) not null,
+	street2 varchar(20) default NULL,
+	PRIMARY KEY (stopId)
 );
 CREATE TABLE ROUTE(
-	ID INT(3) NOT NULL,
-    ROUTE_NAME VARCHAR(10) NOT NULL,
-    STREET1 VARCHAR(50) NOT NULL,
-    STREET2 VARCHAR(50) NOT NULL,
-    PRIMARY KEY (ID, ROUTE_NAME)
+	routeId int(6) not null,
+	route_name VARCHAR(15),
+	S_first_stopId varchar(6) not null,
+	S_last_stopId varchar(6) not null,
+	PRIMARY KEY (routeId),
+	#ToDo: Do these constraints need to be here? What happens when you delete a Route or Stop Id??
+	CONSTRAINT firstStop FOREIGN KEY first_stopId REFERENCES BUS_STOP(stopId),
+	CONSTRAINT firstStop FOREIGN KEY last_stopId REFERENCES BUS_STOP(stopId)
 );
-CREATE TABLE VISIT(
-	ROUTE_ID INT(3) NOT NULL,
-    ROUTE_NAME VARCHAR(10) NOT NULL,
-    BUS_STOP_ID INT(4) UNIQUE NOT NULL,
-    TIME_ARRV TIME NOT NULL,
-    TIME_DPRT TIME NOT NULL,
-    PRIMARY KEY (ROUTE_ID, ROUTE_NAME, BUS_STOP_ID),
-    FOREIGN KEY (ROUTE_ID, ROUTE_NAME) REFERENCES ROUTE(ID, ROUTE_NAME),
-    FOREIGN KEY (BUS_STOP_ID) REFERENCES BUS_STOP(ID)    
+CREATE TABLE VISITS(
+	R_routeId int(6) not null,
+	R_route_name VARCHAR(15),
+	S_stopId varchar(6) not null,
+	arrival_time TIME not null,
+	depart_time TIME not null,
+	PRIMARY KEY (routeId),
+	#ToDo: Do these constraints need to be here? What happens when you delete a Route or Stop Id??
+	CONSTRAINT firstStop FOREIGN KEY first_stopId REFERENCES BUS_STOP(stopId),
+	CONSTRAINT firstStop FOREIGN KEY last_stopId REFERENCES BUS_STOP(stopId)
 );
-CREATE TABLE TRAVERSE(
-	R_NAME VARCHAR(10) NOT NULL,
-    BUS_ID INT(5) NOT NULL,
-    TIME_START TIME NOT NULL,
-    TIME_END TIME NOT NULL,
-    PRIMARY KEY (R_NAME, BUS_ID),
-	FOREIGN KEY (BUS_ID) REFERENCES BUS(ID)    
+CREATE TABLE SCHEDULED(
+	R_route_id int(6) not null,
+	R_route_name VARCHAR(15) NOT NULL,
+    B_busId INT(5),
+    time_start TIME,
+    time_end TIME,
+    PRIMARY KEY (R_route_id, R_route_name, B_busId, time_start),
+	FOREIGN KEY (R_route_id) REFERENCES ROUTE(routeId,
+	FOREIGN KEY (R_route_name) REFERENCES ROUTE(route_name),
+	FOREIGN KEY (B_busId) REFERENCES BUS(busId)
 );
-
-
-
