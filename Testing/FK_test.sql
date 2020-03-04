031e3c28ac4deca75b398f1084dbdab2ade0fad0
Phillip Ovanesyan <phillipov@outlook.com>
2020-03-04 01:12:04 -0800

    Combined all FK tests into one file

--- Testing/FK_test.sql
+++ Testing/FK_test.sql
@@ -1,3 +1,7 @@
+-- Tests all the foreign key constraints
+
+-- PHIL'S TESTS
+
 -- TEST: employee supervisor goes back to employee ssn
 DELETE FROM EMPLOYEE;
 
@@ -39,6 +43,7 @@ DELETE FROM EMPLOYEE;
 
 
 -- TEST: bus E_driver goes back to employee ssn
+DELETE FROM BUS;
 DELETE FROM EMPLOYEE;
 
 INSERT INTO EMPLOYEE (ssn, Fname, Minit, Lname, street, city, state, zip, start_date, supervisor)
@@ -81,6 +86,10 @@ DELETE FROM EMPLOYEE;
 DELETE FROM BUS;
 
 -- taps B_busId goes back to bus busId
+DELETE FROM TAPS;
+DELETE FROM CARD;
+DELETE FROM FARE_TIER;
+DELETE FROM BUS;
 
 INSERT INTO BUS (busId, capacity, start_date, E_driver)
 VALUES (11111, 20, NULL, NULL);
@@ -105,34 +114,233 @@ FROM BUS;
 
 SELECT B_busId, C_card_num, time_stamp
 FROM TAPS;
+	-- shows a bus and a tap record
 
 DELETE FROM TAPS
 WHERE B_busId = 11111;
-	-- remove taps records
-
-DELETE FROM BUS
-WHERE busId = 11111;
-	-- removes the bus this time
+	-- remove tap records
 
 SELECT busId
 FROM BUS;
 
 SELECT B_busId, C_card_num, time_stamp
 FROM TAPS;
-	-- should be no more bus and taps records
+	-- should show a bus and no tap record
 	
-DELETE FROM BUS;
 DELETE FROM TAPS;
+DELETE FROM BUS;
 
 -- taps C_card_num goes back to card cardNum
+DELETE FROM TAPS;
+DELETE FROM BUS;
+DELETE FROM CARD;
+DELETE FROM FARE_TIER;
+
+INSERT INTO FARE_TIER (tier, cost, fare_name)
+VALUES (01, 2.50, NULL);
+
+INSERT INTO CARD (card_number, balance, expiry_date, F_fare)
+VALUES (111111111, 25.00, '2020-12-01', 01);
+
+INSERT INTO BUS (busId, capacity, start_date, E_driver)
+VALUES (11111, 20, NULL, NULL);
+
+INSERT INTO TAPS (B_busId, C_card_num, time_stamp)
+VALUES (11111, 111111111, '2020-03-01 10:00:00');
+	-- bare minimum for a taps and card relationship
+
+DELETE FROM CARD
+WHERE card_number = 111111111;
+	-- try deleting a card
+	-- CURRENT BEHAVIOR: fails by FK constraint to taps
+
+SELECT card_number
+FROM CARD;
+
+SELECT B_busId, C_card_num, time_stamp
+FROM TAPS;
+	-- shows a card and a tap record
+
+DELETE FROM TAPS
+WHERE C_card_num = 111111111;
+	-- remove tap record
+
+SELECT card_number
+FROM CARD;
+
+SELECT B_busId, C_card_num, time_stamp
+FROM TAPS;
+	-- should show a card but no tap records
+
+DELETE FROM TAPS;
+DELETE FROM CARD;
 
 -- card F_fare goes back to faretier tier
+DELETE FROM CARD;
+DELETE FROM FARE_TIER;
+
+INSERT INTO FARE_TIER (tier, cost, fare_name)
+VALUES (01, 2.50, NULL);
+
+INSERT INTO CARD (card_number, balance, expiry_date, F_fare)
+VALUES (111111111, 25.00, '2020-12-01', 01);
+	-- create a card connected to a fare tier
+
+DELETE FROM FARE_TIER
+WHERE tier = 01;
+	-- try removing the fare tier
+	-- CURRENT BEHAVIOR: fails because of an FK constraint to card
+
+SELECT tier
+FROM FARE_TIER;
+
+SELECT card_number
+FROM CARD;
+	-- shows a fare tier and a card with that tier
+
+DELETE FROM CARD
+WHERE card_number = 111111111;
+	-- remove the card
+
+SELECT tier
+FROM FARE_TIER;
+
+SELECT card_number
+FROM CARD;
+	-- should show a fare tier but not card
+
+DELETE FROM CARD;
+DELETE FROM FARE_TIER;
 
 -- visits R_routeId goes back to route routeId
+DELETE FROM VISITS;
+DELETE FROM ROUTE;
+DELETE FROM BUS_STOP;
+
+INSERT INTO BUS_STOP (stopId, stop_name, street1, street2)
+VALUES (222222, 'stop1', 'street1', NULL);
+
+INSERT INTO ROUTE (routeId, route_name, S_first_stopId, S_last_stopId)
+VALUES(111111, NULL, 222222, 222222);
+
+INSERT INTO VISITS (R_routeId, R_route_name, S_stopId, arrival_time, depart_time)
+VALUES(111111, NULL, 222222, '12:00:00', '12:00:30');
+	-- bare minimum to have a visit record for a stop
+
+DELETE FROM ROUTE
+WHERE routeId = 111111;
+	-- try removing a route
+	-- CURRENT BEHAVIOR: fails by FK constraint to visits
+
+SELECT routeId
+FROM ROUTE;
+
+SELECT R_routeId
+FROM VISITS;
+	-- shows a route and a visit record sharing a routeId
+
+DELETE FROM VISITS
+WHERE R_routeId = 111111;
+	-- remove the visit record connected to the route
+
+SELECT routeId
+FROM ROUTE;
+
+SELECT R_routeId
+FROM VISITS;
+	-- should show a route but no visit records
+
+DELETE FROM VISITS;
+DELETE FROM ROUTE;
+DELETE FROM BUS_STOP;
 
 -- visits R_route_name goes back to route route_name
+DELETE FROM VISITS;
+DELETE FROM ROUTE;
+DELETE FROM BUS_STOP;
+
+INSERT INTO BUS_STOP (stopId, stop_name, street1, street2)
+VALUES (222222, 'stop1', 'street1', NULL);
+
+INSERT INTO ROUTE (routeId, route_name, S_first_stopId, S_last_stopId)
+VALUES(111111, 'route1', 222222, 222222);
+
+INSERT INTO VISITS (R_routeId, R_route_name, S_stopId, arrival_time, depart_time)
+VALUES(111111, 'route1', 222222, '12:00:00', '12:00:30');
+	-- route with a non-null name and a visit record
+
+DELETE FROM ROUTE
+WHERE route_name = 'route1';
+	-- try removing a route using its name
+	-- CURRENT BEHAVIOR: fails by FK constraint to visits
+
+SELECT route_Name
+FROM ROUTE;
+
+SELECT R_route_name
+FROM VISITS;
+	-- shows a route and a visit record sharing a routeId
+
+DELETE FROM VISITS
+WHERE R_route_name = 'route1';
+	-- remove the visit record connected to the route
+
+SELECT route_Name
+FROM ROUTE;
+
+SELECT R_route_name
+FROM VISITS;
+	-- should show a route but no visit records
+
+DELETE FROM VISITS;
+DELETE FROM ROUTE;
+DELETE FROM BUS_STOP;
 
 -- visits S_stopId goes back to S_stopId
+DELETE FROM VISITS;
+DELETE FROM ROUTE;
+DELETE FROM BUS_STOP;
+
+INSERT INTO BUS_STOP (stopId, stop_name, street1, street2)
+VALUES (222222, 'stop1', 'street1', NULL);
+
+INSERT INTO ROUTE (routeId, route_name, S_first_stopId, S_last_stopId)
+VALUES(111111, NULL, 222222, 222222);
+
+INSERT INTO VISITS (R_routeId, R_route_name, S_stopId, arrival_time, depart_time)
+VALUES(111111, NULL, 222222, '12:00:00', '12:00:30');
+	-- bus stop and a visit record
+
+DELETE FROM BUS_STOP
+WHERE stopId = 222222;
+	-- try removing the bus stop
+	-- CURRENT BEHAVIOR: fails by FK constraint to route,
+	-- but not notified about visits
+
+SELECT stopId
+FROM BUS_STOP;
+
+SELECT R_routeId
+FROM VISITS;
+	-- using R_routeId and not S_stopId because it's the primary key
+	-- shows a bus stop and its visit record
+
+DELETE FROM VISITS
+WHERE S_stopId = 222222;
+	-- remove the visit record
+
+SELECT stopId
+FROM BUS_STOP;
+
+SELECT R_routeId
+FROM VISITS;
+	-- should show a bus stop without a visit record
+
+DELETE FROM VISITS;
+DELETE FROM ROUTE;
+DELETE FROM BUS_STOP;
+
+-- MITCH'S TESTS
 
 -- route S_first_stop goes back to S_stopId
 INSERT INTO BUS_STOP
