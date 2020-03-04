8c63c5f7e58d398ebc28ca774a151ebffd3f3f7c
Phillip Ovanesyan <phillipov@outlook.com>
2020-03-03 22:57:48 -0800

    Added an FK test for taps-to-bus

--- Testing/FK_test.sql
+++ Testing/FK_test.sql
@@ -1,6 +1,13 @@
 -- TEST: employee supervisor goes back to employee ssn
 DELETE FROM EMPLOYEE;
 
+INSERT INTO EMPLOYEE (ssn, Fname, Minit, Lname, street, city, state, zip, start_date, supervisor)
+VALUES (111111111, 'Super', 'V', 'Isor', '123 1st Street', 'Redmond', 'WA', '98052', '2019-01-01', NULL);
+
+INSERT INTO EMPLOYEE (ssn, Fname, Minit, Lname, street, city, state, zip, start_date, supervisor)
+VALUES (222222222, 'Subo', 'R', 'Dinate', '456 2nd Street', 'Redmond', 'WA', '98052', '2020-02-01', 111111111);
+	-- a manager and their subordinate, linked by an FK
+
 SELECT ssn, supervisor
 FROM EMPLOYEE;
 	-- should show two employees, one managing the other
@@ -30,8 +37,16 @@ FROM EMPLOYEE;
 
 DELETE FROM EMPLOYEE;
 
+
 -- TEST: bus E_driver goes back to employee ssn
 DELETE FROM EMPLOYEE;
+
+INSERT INTO EMPLOYEE (ssn, Fname, Minit, Lname, street, city, state, zip, start_date, supervisor)
+VALUES (111111111, 'Bus', 'D', 'River', '123 1st Street', 'Redmond', 'WA', '98052', '2020-01-01', NULL);
+
+INSERT INTO BUS (busId, capacity, start_date, E_driver)
+VALUES (11111, 20, NULL, 111111111);
+	-- an employee and a bus, linked by an FK because the employee drives the bus
 
 DELETE FROM EMPLOYEE
 WHERE ssn = 111111111;
@@ -66,6 +81,48 @@ DELETE FROM EMPLOYEE;
 DELETE FROM BUS;
 
 -- taps B_busId goes back to bus busId
+
+INSERT INTO BUS (busId, capacity, start_date, E_driver)
+VALUES (11111, 20, NULL, NULL);
+
+INSERT INTO FARE_TIER (tier, cost, fare_name)
+VALUES (01, 2.50, NULL);
+
+INSERT INTO CARD (card_number, balance, expiry_date, F_fare)
+VALUES (111111111, 25.00, '2020-12-01', 01);
+
+INSERT INTO TAPS (B_busId, C_card_num, time_stamp)
+VALUES (11111, 111111111, '2020-03-01 10:00:00');
+	-- bare minimum records for a taps and bus relationship
+
+DELETE FROM BUS
+WHERE busId = 11111;
+	-- try deleting the bus
+	-- CURRENT BEHAVIOR: fails by FK constraint to taps
+
+SELECT busId
+FROM BUS;
+
+SELECT B_busId, C_card_num, time_stamp
+FROM TAPS;
+
+DELETE FROM TAPS
+WHERE B_busId = 11111;
+	-- remove taps records
+
+DELETE FROM BUS
+WHERE busId = 11111;
+	-- removes the bus this time
+
+SELECT busId
+FROM BUS;
+
+SELECT B_busId, C_card_num, time_stamp
+FROM TAPS;
+	-- should be no more bus and taps records
+	
+DELETE FROM BUS;
+DELETE FROM TAPS;
 
 -- taps C_card_num goes back to card cardNum
 
