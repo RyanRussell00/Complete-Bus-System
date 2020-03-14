564ef7af52a3f370ed2fbd4d00db8b9a506b0d1b
Ryan Russell <Ryan.Russell2000@gmail.com>
2020-03-14 11:57:26 -0700

    Added multi column and multiline output support

--- test-harness/venv/auto-tests.py
+++ test-harness/venv/auto-tests.py
@@ -1,16 +1,22 @@
 import mysql.connector;
 import os;
 import fnmatch;
+import re;
 
 '''
 Tests the database to ensure data exists. Expects the database to be populated with test data first.
 '''
 
 
-# Cleans the output of the database. Does not convert it to string.
-# Example: (111222333,) becomes: 111222333
-def clean(line):
-    return '%s' % line;
+# Seperating line purely for display purposes
+def SeperatingLine():
+    print("-------------------------------------------------------------");
+
+
+# Regex to get only the string with dashes, periods, and underscores
+def clean(strIn):
+    result = re.sub("[^a-zA-Z0-9,_'.-]*", "", str(strIn));
+    return result.strip();
 
 
 def readFromFile(path, commands):
@@ -58,40 +64,95 @@ def readFromFile(path, commands):
     return lines;
 
 
+def TablesExist():
+    dbCursor.execute("SHOW TABLES");
+    # add all tables to set to check if they exist later
+    expectedTables = {"BUS", "BUS_STOP", "CARD", "EMPLOYEE", "ADDRESS", "FARE_TIER", "ROUTE", "SCHEDULED", "TAPS",
+                      "VISITS"};
+    count = 0;
+
+    for line in dbCursor:
+        line = '%s' % line;
+        if (line not in expectedTables):
+            print("Found Unexpected Table: " + str(line));
+            return False;
+            SeperatingLine();
+        count += 1;
+
+    # If the total number of tables expected doesnt meet actual counted
+    if (count != len(expectedTables)):
+        print("Number of actual tables doesn't meet number of expected tables");
+        SeperatingLine();
+        return False
+
+    return True;
+
+
 # Runs all commands and queries from given files and tests for expected outputs
 def TestCommands(expectedFile, queriesFile):
     expected = readFromFile(expectedFile, False);
-    assert (expected is not None);
+    # expected = readFromFile("../auto-test-files/Employee.Expected", False);
 
     queries = readFromFile(queriesFile, True);
-    assert (queries is not None);
+    # queries = readFromFile("../auto-test-files/Employee.Tests", True);
 
+    if (expected is not None or queries is not None):
+        print("Failed to read expected outputs or queries commands");
+        SeperatingLine();
+        return False;
+
+    # print("Que");
     # print(queries);
+    # print("Ex");
     # print(expected);
 
-    assert (len(queries) == len(
-        expected)), "Actual queries and expected outputs do not match. Files: " + queriesFile + " and " + expectedFile;
+    # assert (len(queries) == len(
+    #     expected)), "Actual queries and expected outputs do not match. Files: " + queriesFile + " and " + expectedFile;
 
     # Execute each command in the DB and ensure it matches expected output
-    n = 0;
-    while (n < len(queries)):
+    qCount = 0;
+    eCount = 0;
+    while (qCount < len(queries)):
         # try to execute the command, catch any syntax errors and terminate program
         try:
-            dbCursor.execute(queries[n]);
+            dbCursor.execute(queries[qCount]);
         except mysql.connector.ProgrammingError as pe:
-            print("MYSQL ERROR EXECUTING: " + queries[n]);
+            print("MYSQL ERROR EXECUTING: " + queries[qCount]);
             print(pe);
-            print("Terminating Tests.");
-            exit(1);
-        for x in dbCursor:
-            x = clean(x);
-            assert (str(x) == str(expected[n])), "Expected: " + expected[n] + " \n Actual: " + x;
-        n += 1;
+            SeperatingLine();
+            return False;
+
+        for line in dbCursor:
+            if (eCount >= len(expected)):
+                print("Ran out of expected outputs.");
+                SeperatingLine();
+                return False;
+
+            # Clean lines
+            actual = clean(line);
+            expct = clean(expected[eCount]);
+
+            if (actual != expct):
+                print("Actual and Expected dont match.");
+                print("Actual: " + actual);
+                print("Expected: " + expct);
+                SeperatingLine();
+                return False;
+
+            eCount += 1;
+
+            # assert (str(x) == str(expected[n])), "Expected: " + expected[n] + " \n Actual: " + x;
+        qCount += 1;
+    return True;
 
 
 # Imports all the testing files from the given directory.
-# Files must end in '.Expected' or '.Tests' and the prefix must be identical.
+# Files must end in '.Expected' or '.Tests' and the prefix must be identical for pairs of files.
 def RunAllAutoTests(dir):
+    # Check tables exist
+    if (TablesExist() is False):
+        return False;
+
     files = [];
     # Get all files from the directory
     for file in os.listdir(dir):
@@ -104,9 +165,12 @@ def RunAllAutoTests(dir):
                 files.append(dir + "/" + file);
 
     # Ensure that there are an even number of files
-    assert (len(
-        files) % 2 == 0), "Every test file must have a matching expected file. " + "Ensure the files exist and are named properly. \n "
-    "Files must end in 'Expected.txt' or 'Tests.txt' and the prefix must be identical."
+    if (len(files) % 2 == 0):
+        print("Every test file must have a matching expected file. ");
+        print("Ensure the files exist and are named properly.");
+        print("Files must end in 'Expected.txt' or 'Tests.txt' and the prefix must be identical.");
+        SeperatingLine();
+        return False;
 
     # Sort the files to get the matching prefixes next to each other
     files = sorted(files);
@@ -115,7 +179,9 @@ def RunAllAutoTests(dir):
     for f in it:
         n = next(it);
         print("Testing: " + f + " | and | " + n);
-        TestCommands(f, n);
+        # If any of the tests fail stop the tests
+        if (TestCommands(f, n) is False):
+            return False;
 
 
 # This function allows to run tests again without having to restart the entire program.
@@ -123,7 +189,9 @@ def RefreshTests():
     refresh = "Y";
     while (refresh.upper() == "Y"):
         refresh = input("Refresh Testing Files? Y/N: ");
-        RunAllAutoTests("../auto-test-files");
+        if (RunAllAutoTests("../auto-test-files")):
+            print("Some things went wrong. Please view the error messages above.");
+            SeperatingLine();
 
 
 # Gets the database login information from the user and starts the DB connection
@@ -132,6 +200,9 @@ def StartDBConnection():
     password = "";
     host = "";
     databaseName = "";
+
+    print("Welcome to the automated tests for the Complete-Bus-System database. \n");
+    print("If you witness the program crash please take a screenshot and contact your system admin. \n");
 
     print("This version of the tests only work on local MySQL and require you to have the database already created");
     confirm = input("I have the database created on my local MySQL: Y/N: ");
@@ -165,21 +236,24 @@ def StartDBConnection():
         );
         global dbCursor;
         dbCursor = mydb.cursor();
-        assert (dbCursor is not None), "auto-tests: Database connection is null";
-        RefreshTests();
     except mysql.connector.errors.DatabaseError as db:
         print("Error connecting to database. Please View The Error Message and Try Again.");
         print(db);
+        SeperatingLine();
+        # Have user try to login again
         StartDBConnection();
+
+    assert (dbCursor is not None), "auto-tests: Database connection is null";
+
+    RefreshTests();
 
 
 def main():
     # global mydb;
     # global dbCursor;
 
-    StartDBConnection();
-
     # Connect to the DB
+    #
     # mydb = mysql.connector.connect(
     #     user='root',
     #     password='root',
@@ -189,6 +263,11 @@ def main():
     # # Cursor to run commands on the DB
     # global dbCursor;
     # dbCursor = mydb.cursor();
+    #
+    # TestCommands("../auto-test-files/Bus.Expected", "../auto-test-files/Bus.Tests");
+
+    StartDBConnection();
+
     # assert (dbCursor is not None), "auto-tests: Database connection is null";
 
     # RunAllAutoTests("../auto-test-files");
