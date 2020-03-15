2b03a8c9572bb2a1febf520f5213f530079f2e79
Ryan Russell <Ryan.Russell2000@gmail.com>
2020-03-15 11:44:09 -0700

    Modified the employee actions

--- test-harness/venv/CBS/UserInterface.py
+++ test-harness/venv/CBS/UserInterface.py
@@ -82,22 +82,33 @@ def EmployeeQueries():
 
     query = "";
     if (choice == "I"):
-        # ToDo a query that gets all information of the employee including their address (added)
-        query = SELECT_ALL_QUERY % ("*", "EMPLOYEE");
+        # ToDo: Test the query
+        # Join the employee table and the address table to get the full employee's information
+        query = (SELECT_QUERY % ("*", "EMPLOYEE AS E, ADDRESS AS A", "E.ssn = " + ssn + " AND A.E_ssn = " + ssn));
+
+        # query = SELECT_ALL_QUERY % ("*", "EMPLOYEE");
+        # employeeAddressQuery = "SELECT * FROM EMPLOYEE AS E, ADDRESS AS A WHERE E.ssn = " + ssn + " AND A.E_ssn = " + ssn;
+        # addressQuery = SELECT_QUERY % ("*", "ADDRESS", "E_ssn = " + ssn);
+
     elif (choice == "B"):
-        # ToDo a query that gets all busses that employee is assigned to (added)
+        # ToDo: Test the query
+        # Gets all busses that the employee is assigned to
         query = SELECT_QUERY % ("busID, busName", "BUS", "E_driver = " + ssn);
     elif (choice == "R"):
-        # ToDo gets all routes and times that employee is driving. Maybe a limited time frame? (added)
-        query = SELECT_EMPL_ROUTE_QUERY % ("R_routeID, timeStart, timeEnd", "SCHEDULED", "B_busID", "busID", "BUS", "E_driver = " + ssn);
+        # ToDo: Test the query
+        # Gets all routes that the employee is assigned to
+        query = SELECT_EMPL_ROUTE_QUERY % (
+            "R_routeID, timeStart, timeEnd", "SCHEDULED", "B_busID", "busID", "BUS", "E_driver = " + ssn);
     elif (choice == "X"):
         EndProgram();
 
     result = SubmitQuery(query);
-    # global variable accessible anywhere to get Employee's name
-    if (result is None or len(result) != 1):
+    if (result is None):
         print("Error Submitting Query.");
         return None;
+    # If the result is empty that means the system returned an empty set
+    elif (len(result) == 0):
+        print("Empty Set: No values exist for request.")
     else:
         for line in result:
             print(line);
@@ -105,7 +116,6 @@ def EmployeeQueries():
 
 def NewEmployee():
     SeparatingLine();
-    # ToDo: Queries for adding new employees
     # Dictionary (map) for employee's information
     empDict = {"Social Security Number": "",
                "First Name": "",
@@ -119,28 +129,36 @@ def NewEmployee():
         # While the inserted value isn't valid keep prompting user
         while (empDict.get(key) == ""):
             print("Please fill out the Employee's information. If the value is NULL please write 'NULL'. \n"
-                  "Enter 'X' at any time to exit the program.")
+                  "Enter 'X' at any time to exit the program.");
+            nonlocal entry;
             entry = input("Please enter the new Employee's " + key + " :");
             entry = entry.strip();
             # Exit program
             if (entry.upper() == "X"):
                 EndProgram();
             # Ensure the start date follows proper date-time format
-            # ToDo: date is no more than 30 days from today (fixed)
+            # ToDo: Test the date
             if (key == "Start Date (YYYY-MM-DD)"):
                 print("datetime: " + entry);
-                r = re.compile('[0-9]{4}-[0-9]{2}-[0-9]{2}');
-                if r.match(entry) is None:
-                    print(entry + "Didn't match datetime");
+                # r = re.compile('[0-9]{4}-[0-9]{2}-[0-9]{2}');
+                # if r.match(entry) is None:
+                #     print(entry + "Didn't match datetime");
+                #     entry = "";
+                try:
+                    dateObj = datetime.strptime(entry, '%Y-%m-%d').date();
+                except ValueError:
+                    print("Error: Date must be in YYYY-MM-DD format.");
                     entry = "";
-                dateObj = datetime.strptime(entry, '%Y-%m-%d').date();
-                if dateObj - date.today() > timedelta(days = 30):
-                    print("Date must be no more than 30 days from today");
+                    continue;
+                # If the entry isn't empty try to
+                if (entry != "" and dateObj - date.today() > timedelta(days=31)):
+                    print("Date must be no more than 31 days from today");
                     entry = "";
+
             # Make sure the SSN and Super-SSN can be integers.
             # ToDo: Add case where supervisor's SSN is null (fixed)
             if (key == "Social Security Number" or key == "Supervisor SSN"):
-                if (entry != "NULL" and entry != "null"):
+                if (key == "Supervisor SSN" and entry.upper() != "NULL"):
                     try:
                         int(entry);
                         if (len(entry) != 9):
@@ -165,7 +183,8 @@ def NewEmployee():
     if (len(allValues) != 6):
         print("Something went wrong with the new Employee's data");
         return False;
-    query = (NEW_EMPLOYEE_INSERT % (allValues[0], allValues[1], allValues[2], allValues[3], allValues[4], allValues[5]));
+    query = (NEW_EMPLOYEE_INSERT % (
+        allValues[0], allValues[1], allValues[2], allValues[3], allValues[4], allValues[5]));
     print(query);
 
     result = SubmitInsert(query);
@@ -175,33 +194,37 @@ def NewEmployee():
         return False;
     return True;
 
+
+# Gets all stops on a busses route between a given time
 def CheckSchedule():
     SeparatingLine();
-    # ToDo: Queries for checking employees schedule
-    print("Please fill out the Employee's information.\n"
-          "Enter 'X' at any time to exit the program.")
-    entry = input("Employee SSN:");
-    entry = entry.strip();
-    if (entry.upper() == "X"):
-        EndProgram();
-    if (len(entry) == 9):
-        try:
-            int(entry);
-        except ValueError:
-            entry = "";
-    else:
-        entry = "";
-    query = SELECT_EMPL_SCHEDULE % ("timeStart, timeEnd", "SCHEDULED", "B_busID = ", "busID", "BUS", "E_driver = " + entry);
+    # ToDo: Queries for getting all stops on a route and what times the bus is supposed to stop at the stops between a given time.
+
+    # print("Please fill out the Employee's information.\n"
+    #       "Enter 'X' at any time to exit the program.")
+    # entry = input("Employee SSN:");
+    # entry = entry.strip();
+    # if (entry.upper() == "X"):
+    #     EndProgram();
+    # if (len(entry) == 9):
+    #     try:
+    #         int(entry);
+    #     except ValueError:
+    #         entry = "";
+    # else:
+    #     entry = "";
+    query = SELECT_EMPL_SCHEDULE % (
+        "timeStart, timeEnd", "SCHEDULED", "B_busID = ", "busID", "BUS", "E_driver = " + entry);
     result = SubmitQuery(query);
     if (result is False):
         print("Error Submitting Querry.");
         return False;
     return True;
 
+
 def RouteInterface():
     # ToDo: add code for Route interface
 
-    
     return True;
 
 
@@ -233,35 +256,6 @@ def EmployeeInterfaceActions():
     EndProgram();
 
 
-def EmployeeInterface():
-    # SeparatingLine();
-    # global ssn;
-    # ssn = "";
-    # while (ssn.strip() == ""):
-    #     print("Please enter your Social Security Number (9 digits). \n"
-    #           "Or enter X to exit the program.");
-    #     ssn = input("Enter your SSN: ");
-    #
-    # result = [];
-    # if (len(ssn) == 9):
-    #     result = ValidateEmployee(ssn);
-    # elif (ssn.upper() == "X"):
-    #     EndProgram();
-    # else:
-    #     print("Incorrect input. Please make sure SSN is 9 digits long.");
-    #     EmployeeInterface();
-    #
-    # global name
-    # name = "";
-    # if (result is not None):
-    #     name = result[0];
-    # else:
-    #     print("Error getting Employee");
-    #     EmployeeInterface();
-
-    EmployeeInterfaceActions();
-
-
 def EndProgram():
     SeparatingLine()
     CloseConnection();
@@ -285,7 +279,7 @@ def WelcomeUser():
         # ToDo
         PassengerInterface();
     elif (status == "E"):
-        EmployeeInterface();
+        EmployeeInterfaceActions();
     elif (status == "X"):
         EndProgram();
     else:
