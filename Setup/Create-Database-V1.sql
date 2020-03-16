88a996e1856ffe903f05de83cdd91a80b229b326
Phillip Ovanesyan <phillipov@outlook.com>
2020-03-16 11:04:55 -0700

    Added query and tests for finding time frames when buses are at stops

--- Setup/Create-Database-V1.sql
+++ Setup/Create-Database-V1.sql
@@ -19,16 +19,16 @@ CREATE TABLE EMPLOYEE (
     Minit VARCHAR(2) DEFAULT NULL,
     Lname VARCHAR(15) NOT NULL,
     startDate DATE,
-    supervisor INT(9),
+    supervisor INT(9) DEFAULT NULL,
     PRIMARY KEY (ssn),
     FOREIGN KEY (supervisor)
-        REFERENCES EMPLOYEE (ssn) ON DELETE CASCADE ON UPDATE CASCADE
+        REFERENCES EMPLOYEE (ssn) ON DELETE SET NULL -- ON UPDATE CASCADE
 );
 
 CREATE TABLE ADDRESS (
     E_ssn INT(9) NOT NULL UNIQUE,
     street VARCHAR(20) NOT NULL,
-    city VARCHAR(12) NOT NULL,
+    city VARCHAR(30) NOT NULL,
     state CHAR(2) NOT NULL,
     zip CHAR(5) NOT NULL,
     PRIMARY KEY (E_ssn),
@@ -43,23 +43,24 @@ CREATE TABLE BUS (
     E_driver INT(9) UNIQUE DEFAULT NULL,
     PRIMARY KEY (busID),
     FOREIGN KEY (E_driver)
-        REFERENCES EMPLOYEE (ssn) ON DELETE CASCADE ON UPDATE CASCADE
+        REFERENCES EMPLOYEE (ssn) ON DELETE SET NULL ON UPDATE CASCADE
 );
 CREATE TABLE FARE_TIER (
     tier INT(2) NOT NULL,
-    cost FLOAT(4 , 2) NOT NULL,
+    cost FLOAT(4 , 2 ) NOT NULL,
     fareName VARCHAR(9) DEFAULT NULL,
     PRIMARY KEY (tier)
 );
 CREATE TABLE CARD (
-    cardNum INT(9) NOT NULL,
-    balance FLOAT(4 , 2 ) DEFAULT 0,
+    cardNum INT(9) AUTO_INCREMENT NOT NULL,
+    balance FLOAT(6,2) DEFAULT 0,
     expiry_date DATE,
-    F_fare INT(2) NOT NULL,
+    F_fare INT(2) NOT NULL DEFAULT 5,
     PRIMARY KEY (cardNum),
     CONSTRAINT card_fare FOREIGN KEY (F_fare)
-        REFERENCES FARE_TIER (tier) ON DELETE CASCADE ON UPDATE CASCADE
+        REFERENCES FARE_TIER (tier) ON DELETE RESTRICT  ON UPDATE CASCADE
 );
+ALTER TABLE CARD AUTO_INCREMENT = 100000000;
 CREATE TABLE TAPS (
     B_busID INT(5) NOT NULL,
     C_cardNum INT(9) NOT NULL,
@@ -72,8 +73,8 @@ CREATE TABLE TAPS (
 );
 CREATE TABLE BUS_STOP (
     stopID INT(6) NOT NULL,
-    street1 VARCHAR(20) NOT NULL,
-    street2 VARCHAR(20) DEFAULT NULL,
+    street1 VARCHAR(30) NOT NULL,
+    street2 VARCHAR(30) DEFAULT NULL,
     PRIMARY KEY (stopID)
 );
 CREATE TABLE ROUTE (
@@ -84,37 +85,55 @@ CREATE TABLE ROUTE (
     KEY (routeName),
     PRIMARY KEY (routeID, routeName),
     CONSTRAINT route_firstStop FOREIGN KEY (S_firstStop)
-        REFERENCES BUS_STOP (stopID) ON DELETE CASCADE ON UPDATE CASCADE,
+        REFERENCES BUS_STOP (stopID) ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT route_lastStop FOREIGN KEY (S_lastStop)
-        REFERENCES BUS_STOP (stopID) ON DELETE CASCADE ON UPDATE CASCADE
+        REFERENCES BUS_STOP (stopID) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 CREATE TABLE VISITS (
     R_routeID INT(3) NOT NULL,
-	R_routeName VARCHAR(20) NOT NULL,
+    R_routeName VARCHAR(20) NOT NULL,
     S_stopID INT(6) NOT NULL,
-	typeOfDay VARCHAR(10) NOT NULL DEFAULT 'Weekday',
+    typeOfDay VARCHAR(10) NOT NULL DEFAULT 'Weekday',
     arrivalTime TIME NOT NULL,
     departTime TIME NOT NULL,
     PRIMARY KEY (R_routeID, S_stopID, typeOfDay),
-    CONSTRAINT visit_routeID FOREIGN KEY (R_routeID)
+CONSTRAINT visit_routeID FOREIGN KEY (R_routeID)
         REFERENCES ROUTE (routeID) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT visit_routeName FOREIGN KEY (R_routeName)
+    CONSTRAINT visit_routeName FOREIGN KEY (R_routeName)
         REFERENCES ROUTE (routeName) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT visit_stopID FOREIGN KEY (S_stopID)
         REFERENCES BUS_STOP (stopID) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT dayTypes CHECK(typeOfDay IN('Weekday', 'Weekend', 'Holiday', 'Snow'))
+    CONSTRAINT dayTypes CHECK(typeOfDay IN('Weekday', 'Weekend', 'Holiday', 'Snow'))
 );
 CREATE TABLE SCHEDULED (
     R_routeID INT(3) NOT NULL,
-	R_routeName VARCHAR(20) NOT NULL,
+    R_routeName VARCHAR(20) NOT NULL,
     B_busID INT(5),
-    timeStart TIME,
-    timeEnd TIME,
+    timeStart DATETIME,
+    timeEnd DATETIME,
     PRIMARY KEY (R_routeID, R_routeName, B_busID),
     CONSTRAINT scheduled_routeID FOREIGN KEY (R_routeID)
         REFERENCES ROUTE (routeID) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT scheduled_routeName FOREIGN KEY (R_routeName)
+    CONSTRAINT scheduled_routeName FOREIGN KEY (R_routeName)
         REFERENCES ROUTE (routeName) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT scheduled_busID FOREIGN KEY (B_busID)
         REFERENCES BUS (busID) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+INSERT INTO FARE_TIER (tier, cost, fareName)
+VALUES (1, 1, 'Veteran');
+
+INSERT INTO FARE_TIER (tier, cost, fareName)
+VALUES (2, 1, 'Disabled');
+
+INSERT INTO FARE_TIER (tier, cost, fareName)
+VALUES (3, 1.5, 'Student');
+
+INSERT INTO FARE_TIER (tier, cost, fareName)
+VALUES (4, 1.5, 'Child');
+
+INSERT INTO FARE_TIER (tier, cost, fareName)
+VALUES (5, 2.75, 'Adult');
+
+INSERT INTO FARE_TIER (tier, cost, fareName)
+VALUES (6, 1, 'Senior');
---
+++ Testing/bus_query_1.sql
@@ -0,0 +1,65 @@
+DELETE FROM VISITS;
+DELETE FROM ROUTE;
+DELETE FROM BUS_STOP;
+DELETE FROM BUS;
+
+INSERT INTO BUS
+	(busID, capacity, manufactured_date, E_driver)
+VALUES
+	(11111, 20, '2000-01-01', NULL),
+	(22222, 20, '2000-01-01', NULL),
+	(33333, 20, '2000-01-01', NULL),
+	(44444, 20, '2000-01-01', NULL);
+
+INSERT INTO BUS_STOP
+	(stopId, street1, street2)
+VALUES
+	(000001, '1st Street', NULL),
+	(000002, '2nd Street', NULL),
+	(000003, '3rd Street', NULL),
+	(000004, '4th Street', NULL),
+	(000005, '5th Street', NULL),
+	(000006, '6th Street', NULL),
+	(000007, '7th Street', NULL),
+	(000008, '8th Street', NULL);
+
+INSERT INTO ROUTE
+	(routeID, routeName, S_firstStop, S_lastStop)
+VALUES
+	(001, 'Redmond', 000001, 000006),
+	(002, 'Bellevue', 000007, 000008);
+
+INSERT INTO VISITS
+	(R_routeID, R_routeName, S_stopID, typeOfDay, arrivalTime, departTime)
+VALUES
+	(001, 'Redmond', 000001, 'Weekday', '12:00:00', '12:05:00'),
+	(001, 'Redmond', 000002, 'Weekday', '12:06:00', '12:09:00'),
+	(001, 'Redmond', 000003, 'Weekday', '12:10:00', '12:15:00'),
+	(001, 'Redmond', 000004, 'Weekday', '12:16:00', '12:19:00'),
+	(001, 'Redmond', 000005, 'Weekend', '12:00:00', '12:05:00'),
+	(001, 'Redmond', 000006, 'Weekend', '12:06:00', '12:10:00'),
+	(002, 'Bellevue', 000007, 'Weekday', '12:00:00', '12:05:00'),
+	(002, 'Bellevue', 000008, 'Weekday', '12:06:00', '12:10:00');
+
+INSERT INTO SCHEDULED
+	(R_routeID, R_routeName, B_busID, timeStart, timeEnd)
+VALUES
+	(001, 'Redmond', 11111, '2020-03-10 08:00:00', '2020-03-10 11:00:00'),
+	(001, 'Redmond', 22222, '2020-03-10 11:30:00', '2020-03-10 16:00:00'),
+	(002, 'Bellevue', 33333, '2020-03-10 08:00:00', '2020-03-10 11:00:00'),
+	(002, 'Bellevue', 44444, '2020-03-10 11:30:00', '2020-03-10 16:00:00');
+
+SELECT b.busId, DATE(sc.timeStart), v.arrivalTime, v.departTime
+FROM BUS b, VISITS v, SCHEDULED sc
+WHERE v.typeOfDay = 'Weekday'
+	AND sc.B_busID = b.busID
+	AND sc.R_routeID = v.R_routeID
+	AND sc.R_routeName = v.R_routeName
+	AND TIME(sc.timeStart) <= v.arrivalTime
+	AND TIME(sc.timeEnd) >= v.departTime
+ORDER BY b.busID ASC, v.arrivalTime ASC;
+
+DELETE FROM VISITS;
+DELETE FROM ROUTE;
+DELETE FROM BUS_STOP;
+DELETE FROM BUS;
